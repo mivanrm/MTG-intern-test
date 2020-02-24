9d662c11bccf77c7539f0ef6cbabfdb0e01b4251
mivanrm <mivanrm12@gmail.com>
2020-02-24 15:26:11 +0700

    init project

---
+++ api/routes/users.js
@@ -0,0 +1,8 @@
+import express from 'express'
+var userController= require('../../controllers/UserController.js')
+
+modules.exports{
+    var routes= express.Router
+    router.get('/users',)
+    app.use('/api/v1',router)
+}
